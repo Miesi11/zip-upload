f5752e4b8e158d8768c455881ef0669a37ac588a
Miesi11 <michaelmiesenboeck@gmail.com>
2025-09-03 04:46:31 -0700

    Add brown color to Tailwind config for GitHub Pages build

--- 2025-09-01/tailwind.config.js
+++ 2025-09-01/tailwind.config.js
@@ -3,20 +3,25 @@ module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
+    
   ],
   theme: {
     extend: {
-      colors: {
-        primary: {
-          light: '#f5f5ed',
-          DEFAULT: '#e3d8b7',
-          dark: '#b9a583',
-        },
-        accent: {
-          DEFAULT: '#927240',
-          dark: '#7a5a2c',
-        },
+       colors: {
+      primary: {
+        light: '#f5f5ed',
+        DEFAULT: '#e3d8b7',
+        dark: '#b9a583',
       },
+      accent: {
+        DEFAULT: '#9272a0',
+        dark: '#7a5a2c',
+      },
+      brown: {
+        900: '#7a5a2c',
+      },
+    },
+  },
       fontFamily: {
         handwriting: ['"Caveat"', 'ui-sans-serif', 'system-ui'],
       },
